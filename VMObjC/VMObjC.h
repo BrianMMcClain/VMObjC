2e27cd0972287a276f31a71d3a3b035f2def80a8
Brian McClain <brianmmcclain@gmail.com>
2012-06-27 22:42:53 -0400

    Initial Commit

---
+++ VMObjC/VMObjC.h
@@ -0,0 +1,13 @@
+//
+//  VMObjC.h
+//  VMObjC
+//
+//  Created by Brian McClain on 6/27/12.
+//  Copyright (c) 2012 Brian McClain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VMObjC : NSObject
+
+@end
---
+++ VMObjC/VMObjC.m
@@ -0,0 +1,13 @@
+//
+//  VMObjC.m
+//  VMObjC
+//
+//  Created by Brian McClain on 6/27/12.
+//  Copyright (c) 2012 Brian McClain. All rights reserved.
+//
+
+#import "VMObjC.h"
+
+@implementation VMObjC
+
+@end
